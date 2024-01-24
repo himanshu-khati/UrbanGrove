a5b893c2924f8916823b7d5b16a86f02335bec7b
himanshu-khati <himanshukhati94@gmail.com>
2024-01-24 19:05:24 +0530

    product controller/model

---
+++ backend/models/productModel.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Product Name"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter product description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter Product price"],
+    maxLength: [8, "price cannot exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "please enter product category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter Product stock"],
+    maxLength: [4, "stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
