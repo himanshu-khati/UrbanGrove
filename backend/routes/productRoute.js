a0e4649b39c00fdbccd1b2a9513d6f849a708c37
himanshu-khati <himanshukhati94@gmail.com>
2024-01-28 09:59:03 +0530

    user routes completed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,6 +6,8 @@ const {
   deleteProduct,
   getProductDetails,
   createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/productController");
 const { isAuthenticatedUser, authorisedRoles } = require("../middleware/auth");
 const router = express.Router();
@@ -20,4 +22,8 @@ router
   .delete(isAuthenticatedUser, authorisedRoles("admin"), deleteProduct);
 router.route("/product/:id").get(getProductDetails);
 router.route("/review").put(isAuthenticatedUser, createProductReview);
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 module.exports = router;
